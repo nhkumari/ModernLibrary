375c5f43174d8d4d1b7b680ab9425560a285bb71
Neha Kumari <nehkumari@paypal.com>
2021-11-18 19:23:10 +0530

    deleted target folder

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-  	INSERT INTO
-    	TBL_MODERN_BOOKS (id, title,copies)
-    VALUES
-      	(1, 'Lets Us C',3),
-      	(2, 'Evolution of java',6),
-      	(3, 'Champak',1),
-      	(4, 'Python',2);
