40014a98b7aee884b17cfbcdd4575ce121994727
Neha Kumari <nehkumari@paypal.com>
2021-11-22 10:58:33 +0530

    DTPZN-4 completed user story 4, return books

--- src/main/java/com/neha/controller/ModernLibraryController.java
+++ src/main/java/com/neha/controller/ModernLibraryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,11 @@ public class ModernLibraryController {
     @Autowired
     private ModernLibraryService service;
 
+    /**
+     * This will return all unassigned books, a book which is already assigned to a user
+     * will not be shown in the library
+     * user-story-1 <View All Books>
+     */
     @RequestMapping(method = RequestMethod.GET, value = "/books")
     public ResponseEntity getAllBooks() {
         ResponseEntity rs = null;
@@ -32,6 +38,10 @@ public class ModernLibraryController {
         return new ResponseEntity<>(books, HttpStatus.OK);
     }
 
+    /**
+     * This api is for borrowing a book by passing user-id
+     * user-story-2 <Borrow a Book>
+     **/
     @RequestMapping(method = RequestMethod.GET, value = "/borrow/{user_id}/{book_id}")
     public ResponseEntity<Object> borrowBooks(@PathVariable("user_id") long userId,
             @PathVariable("book_id") long bookId) {
@@ -50,6 +60,10 @@ public class ModernLibraryController {
         return new ResponseEntity<>("invalid data", HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * This api is for borrowing a book by passing isbn number of the book
+     * user-story-3 <Borrow a Copy>
+     **/
     @RequestMapping(method = RequestMethod.GET, value = "/borrow_copy/{user_id}/{isbn}")
     public ResponseEntity<Object> borrowACopy(@PathVariable("user_id") long userId,
             @PathVariable("isbn") String isbn) {
@@ -70,6 +84,31 @@ public class ModernLibraryController {
         return new ResponseEntity<>("invalid data/book is not available", HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * This api is for returning one/multiple borrowed books by passing isbn list
+     * user-story-4 <Return Book>
+     **/
+    @RequestMapping(method = RequestMethod.POST, value = "/return_books/{user_id}")
+    public ResponseEntity<Object> returnBooks(@PathVariable("user_id") long userId,
+            @RequestBody List<String> isbnList) {
+        Optional<User> user = service.findUser(userId);
+        if (user.isPresent()) {
+            List<Book> bookList = new ArrayList<>();
+            for (String isbn : isbnList) {
+                List<Book> isbnBookList = service.findAssignedCopies(user.get().getId(), isbn);
+                if (isbnBookList.isEmpty()) {
+                    return new ResponseEntity<>("couldn't perform operation, book with isbn: " + isbn + ", is not "
+                            + "assigned to requested user",
+                            HttpStatus.BAD_REQUEST);
+                }
+                bookList.addAll(isbnBookList);
+            }
+            return updateBorrowList(user.get(), bookList);
+        }
+
+        return new ResponseEntity<>("requested user doesn't exists", HttpStatus.BAD_REQUEST);
+    }
+
     private ResponseEntity<Object> assignBook(Book book, User user) {
         List<Book> bookList = service.findBooksByUserId(user.getId());
         if (bookList.isEmpty()) {
@@ -79,6 +118,15 @@ public class ModernLibraryController {
             return new ResponseEntity<>("borrow limit exceeded", HttpStatus.BAD_REQUEST);
         }
         bookList.add(book);
+        user.setBookList(bookList);
+        service.updateUser(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    private ResponseEntity<Object> updateBorrowList(User user, List<Book> booksToReturn) {
+
+        List<Book> bookList = service.findBooksByUserId(user.getId());
+        bookList.removeAll(booksToReturn);
         user.setBookList(bookList);
         service.updateUser(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
